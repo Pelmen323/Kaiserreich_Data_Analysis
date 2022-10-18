3f3f5c7aaf0a71c137eec8b89342f9237649d987
Pelmen323 <nozovka9@mail.ru>
2022-10-18 16:57:33 +0400

    Update app:
    - Add "Second Weltkrieg" section, pie and graph

--- app.py
+++ app.py
@@ -95,6 +95,129 @@ def read_csv_file(input_file: str) -> pd.DataFrame:
 ####################################
 # Functions for separate questions #
 ####################################
+@app.callback(
+    Output("2wk-winrate-pie", "figure"),
+    Input("2wk-winrate-data-source", "value"),
+    Input("2wk-winrate-war-configuration", "value"))
+@cache.memoize(timeout=TIMEOUT)
+def create_2wk_winrate_pie(input_file, war_configuration):
+    """The Second Weltkrieg winrate pie
+
+    Args:
+        input_file (_type_): .csv file name imported by the function. Is defined by the dropdown value
+
+    Returns:
+        fig: graph object
+    """
+    import_doc = read_csv_file(input_file)
+
+
+    if "Who won the Franco-German part of the 2nd Weltkrieg?" in import_doc.columns:
+        if war_configuration == "Germany-France":
+            import_doc = import_doc["Who won the Franco-German part of the 2nd Weltkrieg?"].value_counts()
+        elif war_configuration == "Germany-Russia":
+            import_doc = import_doc["Who won the Russo-German part of the 2nd Weltkrieg?"].value_counts()
+        else:
+            return generate_mock_graph()
+        df = pd.DataFrame({"Country": import_doc.index, "Value": import_doc.values})
+        fig = px.pie(
+            data_frame=df,
+            values="Value",
+            names="Country",
+            title="2WK winrate pie",
+            color="Country",
+            color_discrete_map={
+                "Reichspakt": "rgb(93,93,61)",
+                "Internationale": "rgb(10,54,175)",
+                "Russia": "rgb(0,127,14)",
+                "Socialist Russia": "rgb(175,13,0)",
+                "Nobody": "grey",
+            },
+        )
+
+        fig.update_layout(
+            plot_bgcolor=colors["background"],
+            paper_bgcolor=colors["background"],
+            font_color=colors["text"],
+            legend_title="Country",
+
+        )
+
+        return fig
+    else:
+        return generate_mock_graph()
+
+
+@app.callback(
+    Output("2wk-winrate-graph", "figure"),
+    Input("2wk-winrate-data-source", "value"))
+@cache.memoize(timeout=TIMEOUT)
+def create_2wk_winrate_graph(input_file):
+    """2WK winrate graph
+
+    Args:
+        input_file (_type_): .csv file name imported by the function. Is defined by the dropdown value
+
+    Returns:
+        fig: graph object
+    """
+    import_doc = read_csv_file(input_file)
+
+    if "If the Reichspakt lost the 2nd Weltkrieg, when did they fall?" in import_doc.columns and "Who won the Franco-German part of the 2nd Weltkrieg?" in import_doc.columns:
+        import_doc_wr_over_time = pd.DataFrame(columns=["Year", "Internationale", "Reichspakt", "Nobody"])
+        x = import_doc["If the Reichspakt lost the 2nd Weltkrieg, when did they fall?"]
+        # Create a single Series with war ending date - since the question is split in 2, we need to unify data to create a graph
+        for i, value in enumerate(x):
+            if pd.isnull(value):
+                if pd.isnull(import_doc["If the Internationale lost the 2nd Weltkrieg, when did France fall?"][i]):
+                    x[i] = "Did not end"
+                else:
+                    x[i] = import_doc["If the Internationale lost the 2nd Weltkrieg, when did France fall?"][i]
+
+        # Convert float values to int
+        for i, value in enumerate(x):
+            if isinstance(value, float):
+                x[i] = int(x[i])
+
+        # Convert series data type to str
+        x = pd.Series(data=x, dtype="string")
+
+        for i in x.value_counts().index:
+            internationale = len(import_doc[import_doc["If the Reichspakt lost the 2nd Weltkrieg, when did they fall?"] == i][import_doc["Who won the Franco-German part of the 2nd Weltkrieg?"] == 'Internationale'])
+            reichspakt = len(import_doc[import_doc["If the Reichspakt lost the 2nd Weltkrieg, when did they fall?"] == i][import_doc["Who won the Franco-German part of the 2nd Weltkrieg?"] == 'Reichspakt'])
+            nobody = len(import_doc[import_doc["If the Reichspakt lost the 2nd Weltkrieg, when did they fall?"] == i][import_doc["Who won the Franco-German part of the 2nd Weltkrieg?"] == 'Nobody'])
+            new_row = pd.Series({"Year": i, "Internationale": internationale, "Reichspakt": reichspakt, "Nobody": nobody})
+            import_doc_wr_over_time = import_doc_wr_over_time.append(new_row, ignore_index=True)
+
+        prepared_data = import_doc_wr_over_time.sort_values(by="Year")
+        px.line(data_frame=prepared_data, x='Year', y=['Internationale', 'Reichspakt', 'Nobody'], labels={'x':'Date', 'y':"Country"})
+        fig = px.line(
+            data_frame=prepared_data,
+            x="Year",
+            y=['Internationale', 'Reichspakt'],
+            labels={"x": "Date", "y": "Country"},
+            title="FRA-GER winrate graph",
+            color="variable",
+            color_discrete_map={
+                "Reichspakt": "rgb(93,93,61)",
+                "Internationale": "rgb(10,54,175)",
+                "Nobody": "grey",
+            },
+        )
+
+        fig.update_layout(
+            plot_bgcolor=colors["background"],
+            paper_bgcolor=colors["background"],
+            font_color=colors["text"],
+            legend_title="Country",
+        )
+        fig.update_traces(line=dict(width=line_widths["plot_line"]))
+        fig.update_xaxes(showgrid=True, gridwidth=line_widths["grid_xaxis"], gridcolor=colors["grid"])
+        fig.update_yaxes(showgrid=True, gridwidth=line_widths["grid_yaxis"], gridcolor=colors["grid"])
+
+        return fig
+    else:
+        return generate_mock_graph()
 
 
 @app.callback(
@@ -204,12 +327,12 @@ def create_scw_winrate_pie(input_file):
     Input("acw-winrate-data-source", "value"),
     Input("acw-winrate-war-configuration", "value"))
 @cache.memoize(timeout=TIMEOUT)
-def create_acw_winrate_pie(input_file, acw_war_configuration):
+def create_acw_winrate_pie(input_file, war_configuration):
     """American Civil War winrate pie
 
     Args:
         input_file (_type_): .csv file name imported by the function. Is defined by the dropdown value
-        acw_war_configuration (str): Graph option
+        war_configuration (str): Graph option
 
     Returns:
         fig: graph object
@@ -217,22 +340,20 @@ def create_acw_winrate_pie(input_file, acw_war_configuration):
     import_doc = read_csv_file(input_file)
 
     if "When did the American Civil War end?" in import_doc.columns:
-        if acw_war_configuration == "All":
+        if war_configuration == "All":
             if "Who won the American Civil War?" in import_doc.columns:
                 import_doc = import_doc["Who won the American Civil War?"].value_counts()
             else:
                 return generate_mock_graph()
-
-        elif acw_war_configuration == "2-Way War":
+        elif war_configuration == "2-Way War":
             import_doc = import_doc["If the American Civil War was a two-way, who won it?"].value_counts()
-
-        elif acw_war_configuration == "3-Way War":
+        elif war_configuration == "3-Way War":
             import_doc = import_doc["If the American Civil War was a three-way, who won it?"].value_counts()
-        elif acw_war_configuration == "Mac Goes East":
+        elif war_configuration == "Mac Goes East":
             import_doc = import_doc["If MacArthur retreated EAST, who won the ACW?"].value_counts()
-        elif acw_war_configuration == "Mac Goes West":
+        elif war_configuration == "Mac Goes West":
             import_doc = import_doc["If MacArthur retreated WEST, who won the ACW?"].value_counts()
-        elif acw_war_configuration == "Mac Doesn't Retreat":
+        elif war_configuration == "Mac Doesn't Retreat":
             import_doc = import_doc["If MacArthur did NOT retreat, who won the ACW?"].value_counts()
 
         df = pd.DataFrame({"Country": import_doc.index, "Value": import_doc.values})
@@ -269,12 +390,12 @@ def create_acw_winrate_pie(input_file, acw_war_configuration):
     Input("acw-winrate-data-source", "value"),
     Input("acw-winrate-war-configuration", "value"))
 @cache.memoize(timeout=TIMEOUT)
-def create_acw_winrate_graph(input_file, acw_war_configuration):
+def create_acw_winrate_graph(input_file, war_configuration):
     """American Civil War winrate graph
 
     Args:
         input_file (_type_): .csv file name imported by the function. Is defined by the dropdown value
-        acw_war_configuration (str): Graph option
+        war_configuration (str): Graph option
 
     Returns:
         fig: graph object
@@ -283,7 +404,7 @@ def create_acw_winrate_graph(input_file, acw_war_configuration):
     import_doc_wr_over_time = pd.DataFrame(data=import_doc, columns=["Year", "USA", "CSA", "TEX", "PSA", "NEE", "Nobody"])
 
     if "When did the American Civil War end?" in import_doc.columns:
-        if acw_war_configuration == "All":
+        if war_configuration == "All":
             if "Who won the American Civil War?" in import_doc.columns:
                 for i in import_doc["When did the American Civil War end?"].value_counts().index:
                     usa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["Who won the American Civil War?"] == "USA"])
@@ -297,7 +418,7 @@ def create_acw_winrate_graph(input_file, acw_war_configuration):
             else:
                 return generate_mock_graph()
 
-        elif acw_war_configuration == "2-Way War":
+        elif war_configuration == "2-Way War":
             for i in import_doc["When did the American Civil War end?"].value_counts().index:
                 usa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If the American Civil War was a two-way, who won it?"] == 'USA'])
                 csa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If the American Civil War was a two-way, who won it?"] == 'CSA'])
@@ -308,7 +429,7 @@ def create_acw_winrate_graph(input_file, acw_war_configuration):
                 new_row = pd.Series({"Year": i, "USA": usa, "CSA": csa, "TEX": tex, "PSA": psa, "NEE": nee, "Nobody": nobody})
                 import_doc_wr_over_time = import_doc_wr_over_time.append(new_row, ignore_index=True)
 
-        elif acw_war_configuration == "3-Way War":
+        elif war_configuration == "3-Way War":
             for i in import_doc["When did the American Civil War end?"].value_counts().index:
                 usa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If the American Civil War was a three-way, who won it?"] == 'USA'])
                 csa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If the American Civil War was a three-way, who won it?"] == 'CSA'])
@@ -319,7 +440,7 @@ def create_acw_winrate_graph(input_file, acw_war_configuration):
                 new_row = pd.Series({"Year": i, "USA": usa, "CSA": csa, "TEX": tex, "PSA": psa, "NEE": nee, "Nobody": nobody})
                 import_doc_wr_over_time = import_doc_wr_over_time.append(new_row, ignore_index=True)
 
-        elif acw_war_configuration == "Mac Goes East":
+        elif war_configuration == "Mac Goes East":
             for i in import_doc["When did the American Civil War end?"].value_counts().index:
                 usa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If MacArthur retreated EAST, who won the ACW?"] == 'USA'])
                 csa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If MacArthur retreated EAST, who won the ACW?"] == 'CSA'])
@@ -330,7 +451,7 @@ def create_acw_winrate_graph(input_file, acw_war_configuration):
                 new_row = pd.Series({"Year": i, "USA": usa, "CSA": csa, "TEX": tex, "PSA": psa, "NEE": nee, "Nobody": nobody})
                 import_doc_wr_over_time = import_doc_wr_over_time.append(new_row, ignore_index=True)
 
-        elif acw_war_configuration == "Mac Goes West":
+        elif war_configuration == "Mac Goes West":
             for i in import_doc["When did the American Civil War end?"].value_counts().index:
                 usa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If MacArthur retreated WEST, who won the ACW?"] == 'USA'])
                 csa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If MacArthur retreated WEST, who won the ACW?"] == 'CSA'])
@@ -341,7 +462,7 @@ def create_acw_winrate_graph(input_file, acw_war_configuration):
                 new_row = pd.Series({"Year": i, "USA": usa, "CSA": csa, "TEX": tex, "PSA": psa, "NEE": nee, "Nobody": nobody})
                 import_doc_wr_over_time = import_doc_wr_over_time.append(new_row, ignore_index=True)
 
-        elif acw_war_configuration == "Mac Doesn't Retreat":
+        elif war_configuration == "Mac Doesn't Retreat":
             for i in import_doc["When did the American Civil War end?"].value_counts().index:
                 usa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If MacArthur did NOT retreat, who won the ACW?"] == 'USA'])
                 csa = len(import_doc[import_doc["When did the American Civil War end?"] == i][import_doc["If MacArthur did NOT retreat, who won the ACW?"] == 'CSA'])
@@ -394,14 +515,22 @@ app.layout = html.Div(
     children=[
         html.Label(children="Links to items", id="contents-header"),
         html.Br(),
+        html.A(children="The Second Weltkrieg", href="#2wk-section", className="contents-link"),
+        html.Br(),
         html.A(children="Argentina-Chile", href="argentina-section", className="contents-link"),
         html.Br(),
         html.A(children="Spanish Civil War", href="#scw-section", className="contents-link"),
         html.Br(),
         html.A(children="American Civil War", href="#acw-section", className="contents-link"),
         html.Br(),
-        html.Label(children="Argentinean-Chilean War", id="argentina-section"),
 
+        html.Label(children="The Second Weltkrieg", id="2wk-section"),
+        dcc.Dropdown(id="2wk-winrate-data-source", options=get_dropdown_options(), value=get_dropdown_options()[0], clearable=False),
+        dcc.Dropdown(id="2wk-winrate-war-configuration", options=["Germany-France", "Germany-Russia"], value="Germany-France", clearable=False),
+        dcc.Graph(id="2wk-winrate-pie"),
+        dcc.Graph(id="2wk-winrate-graph"),
+        
+        html.Label(children="Argentinean-Chilean War", id="argentina-section"),
         dcc.Dropdown(id="argentina-winrate-data-source", options=get_dropdown_options(), value=get_dropdown_options()[0], clearable=False),
         dcc.Graph(id="argentina-winrate-graph"),
         html.Br(),
